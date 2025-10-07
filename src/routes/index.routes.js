af61f97296c62062071085530a4d850515006eb1
vinicius valverde <vinicius.valverde@aluno.senai.br>
2025-10-07 01:27:35 -0300

    delete shield and managerTeam and populating database

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,17 +3,12 @@ import express from "express";
 // Importar todas as rotas
 import titleRouter from "./titleRoutes.js";
 import playerRouter from "./playerRoutes.js";
-import shieldRouter from "./shieldRoutes.js";
 import legendRouter from "./legendRoutes.js";
-import managerTeamRouter from "./managerTeamRoutes.js";
 
 const router = express.Router();
 
-// Rotas públicas
 router.use("/titles", titleRouter);
-router.use("/shields", shieldRouter);
 router.use("/players", playerRouter);
-router.use("/managerTeam", managerTeamRouter);
 router.use("/legends", legendRouter);
 
 export default router;
